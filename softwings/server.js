7b0dc621f7ca17bce3c44c975bfb6c29e5b66092
varshini sivan <varshinidharumapri@gmail.com>
2026-02-10 23:20:03 +0530

    backend setup

---
+++ softwings/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Softwings backend running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () =>
+      console.log("Server running on http://localhost:5000")
+    );
+  })
+  .catch((err) => console.error(err));
