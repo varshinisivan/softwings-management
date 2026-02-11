a66bcfb98efbeba646cdfa00184ca8144b2058e1
varshini sivan <varshinidharumapri@gmail.com>
2026-02-11 16:31:32 +0530

    Add backend folder as normal files

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Softwings backend running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => {
+      console.log("Server running on http://localhost:5000");
+    });
+  })
+  .catch((err) => console.error("MongoDB error:", err));
