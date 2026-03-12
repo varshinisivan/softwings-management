1259b8bbf42ecae2d4e6b2d092fd79ec901ff5b3
varshini sivan <varshinidharumapri@gmail.com>
2026-03-12 07:42:16 +0530

    fix client controller path

--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const clientController = require("../controllers/clientController");
+const clientController = require("../controllers/clientcontroller");
 
 
 // =====================================================
